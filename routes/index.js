a68e523fcee499ed7b2cf8b55cb68bcc431839e0
kabore r evariste <kaboreva@gmail.com>
2023-11-15 17:01:18 +0100

    mise à jour 2

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get("/movies", (req, res) => {
     fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`)
         .then( response => response.json())
         .then( data => {
-            res.json({movies:data.results}); 
+            res.json({movies: data.results}); 
         })
 }); 
 
